ed104f228a8101358ddb37c78d8187a9d063d2b7
majihao <1916634896@qq.com>
2024-07-17 20:36:24 +0800

    fix:add.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import threading
-# import time
-# m={'a':1,"b":2}
-# for name,ite in m.items():
-#     print(name,ite)
-# print("All threads have been started.")
+import time
+m={'a':1,"b":2}
+for name,ite in m.items():
+    print(name,ite)
+print("All threads have been started.")
 a=[]
 c=[0,1,2,3,4]
 d=[7,8]
