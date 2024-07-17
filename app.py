6d20d51d32a3ed40ce2ee483831d299e4e968fdb
majihao <1916634896@qq.com>
2024-07-17 20:23:17 +0800

    Initial

---
+++ app.py
@@ -0,0 +1,11 @@
+import threading
+# import time
+# m={'a':1,"b":2}
+# for name,ite in m.items():
+#     print(name,ite)
+# print("All threads have been started.")
+a=[]
+c=[0,1,2,3,4]
+d=[7,8]
+a.extend(d)
+print(a)
